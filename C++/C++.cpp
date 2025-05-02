57701ce33a1b878dafb68163a96ab927fab5ccae
Igor Rodrigues Machado <107290125+igorrm19@users.noreply.github.com>
2025-05-02 20:43:22 -0300

    Update C++.cpp

--- C++/C++.cpp
+++ C++/C++.cpp
@@ -1,35 +1,64 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="C++" />
-		<Option pch_mode="2" />
-		<Option compiler="icc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/C++" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="icc" />
-				<Compiler>
-					<Add option="/Zi" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/C++" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="icc" />
-				<Compiler>
-					<Add option="/O2" />
-				</Compiler>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-			<Add option="/EHs" />
-		</Compiler>
-		<Unit filename="main.cpp" />
-		<Extensions />
-	</Project>
-</CodeBlocks_project_file>
+#include <iostream>
+#include <deque>
+#include <list>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+enum TipoVeiculo { COMUM, PRIORITARIO, EMERGENCIA, SERVICO };
+
+struct Veiculo {
+    string placa;
+    TipoVeiculo tipo;
+
+    Veiculo(string p, TipoVeiculo t) : placa(p), tipo(t) {}
+};
+
+string tipoToString(TipoVeiculo tipo) {
+    switch (tipo) {
+        case COMUM: return "Comum";
+        case PRIORITARIO: return "Prioritário";
+        case EMERGENCIA: return "Emergência";
+        case SERVICO: return "Serviço";
+        default: return "Desconhecido";
+    }
+}
+
+const int MAX_VAGAS = 10;
+vector<Veiculo*> vagas(MAX_VAGAS, nullptr);
+deque<Veiculo> filaEntrada;
+deque<Veiculo> filaSaida;
+list<Veiculo> estacionamento;
+
+void adicionarVeiculo() {
+    string placa;
+    int tipo;
+
+    cout << "Placa: ";
+    cin >> placa;
+
+    cout << "Tipo (0-Comum, 1-Prioritário, 2-Emergência, 3-Serviço): ";
+    cin >> tipo;
+
+    if (tipo < 0 || tipo > 3) {
+        cout << "Tipo inválido!" << endl;
+        return;
+    }
+
+    Veiculo v(placa, static_cast<TipoVeiculo>(tipo));
+
+    if (v.tipo == EMERGENCIA) {
+        filaEntrada.push_front(v);
+    } else if (v.tipo == PRIORITARIO) {
+        auto it = filaEntrada.begin();
+        while (it != filaEntrada.end() && it->tipo == EMERGENCIA)
+            ++it;
+        filaEntrada.insert(it, v);
+    } else {
+        filaEntrada.push_back(v);
+    }
+
+    cout << "Veículo " << placa << " adicionado à fila de entrada." << endl;
+}
